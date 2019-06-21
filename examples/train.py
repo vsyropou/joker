e3158802d8b732f14f2d73fd86c089c68eb4c91f
vsyropou <vsyropou5@gmail.com>
2019-06-21 14:50:38 +0200

    reshaped data in 5D (strugling with dynamic cnn inputs...)

--- examples/train.py
+++ examples/train.py
@@ -20,9 +20,22 @@ batch_size = -1
 msg_srvc = MessageService(print_level = 2)# if verbose else 1)
 dbs_srvc = instansiate_engine('services.postgres', 'PostgresWriterService')
 
+
+# configure network
+n_cnn_filters = 64
+embedings_kernel_size = 4
+
+time_sequence_length = 2
+batch_length = 5
+
+text_sequence_length = 100
+embeddings_vector_length = 2
+
+
+
 # data streams
 coin_cols = ['price as price_zero',
-             'price as price_one',
+             'lead(price, 1, NULL) OVER (PARTITION BY coin_id ORDER BY insertion_time) as price_one',
              'percent_change_24h',
              'time_idx']
 
@@ -42,23 +55,23 @@ twt_ftrs_cols = ['favorite_count',
 
 btc_stream = SqlReadStreamer(dbs_srvc,
                              crypto_features_qry(coin_cols, 'bitcoin'),
-                             step=batch_size,
+                             step=-1,
                              records_format='tuple')
 
 ltc_stream = SqlReadStreamer(dbs_srvc,
                              crypto_features_qry(coin_cols, 'litecoin'),
-                             step=batch_size,
+                             step=-1,
                              records_format='tuple')
 
 twt_stream = SqlReadStreamer(dbs_srvc,
                              tweet_visible_features_qry(twt_cols),
-                             step=batch_size,
+                             step=-1,
                              records_format='list')
 
-# emb_stream = SqlReadStreamer(dbs_srvc,
-#                              embeddings_matrix_qry(25, 'embedings_glove25'),
-#                              step=batch_size,
-#                              records_format='nparray')
+emb_stream = SqlReadStreamer(dbs_srvc,
+                             embeddings_matrix_qry(25, 'embedings_glove25'),
+                             step=-1,
+                             records_format='nparray')
 
 # coin data
 btc_data = pd.DataFrame([i for i in btc_stream()], columns=coin_cols)
@@ -68,27 +81,31 @@ ltc_data = pd.DataFrame([i for i in ltc_stream()], columns=coin_cols)
 btc_data.index = btc_data['time_idx']
 ltc_data.index = ltc_data['time_idx']
 
-# tweet data
-twt_data = pd.DataFrame([i for i in twt_stream()], columns=twt_cols)
 
-twt_text = twt_data[['embeding_keys_array','tft.time_idx']].groupby('tft.time_idx').agg(lambda tweets: [emb  for t in tweets for emb in t] )
+# # tweet data
+# twt_data = pd.DataFrame([i for i in twt_stream()], columns=twt_cols)
 
-twt_ftrs = twt_data[twt_ftrs_cols+['tft.time_idx']].groupby('tft.time_idx').agg(np.mean)
+# twt_ftrs = twt_data[twt_ftrs_cols+['tft.time_idx']].groupby('tft.time_idx').agg(np.mean)
 
-twt_ftrs = pd.DataFrame(StandardScaler().fit_transform(twt_ftrs), columns=twt_ftrs_cols, index=twt_ftrs.index)
+# twt_ftrs = pd.DataFrame(StandardScaler().fit_transform(twt_ftrs), columns=twt_ftrs_cols, index=twt_ftrs.index)
 
-# embedings
-# emb_table = pd.DataFrame([i for i in emb_stream()], columns=['idx','word', 'embeddings'])
-
-# pca = PCA(n_components=1)
-# emb_table_reduced = pd.DataFrame(pca.fit_transform([*emb_table['embeddings']]))
 
 # # embedded tweets
-# embedded_tweets = twt_text['embeding_keys_array'].apply(lambda tokn_twt: [emb_table_reduced.loc[idx][0] for idx in tokn_twt])
+# emb_table = pd.DataFrame([i for i in emb_stream()], columns=['idx','word', 'embeddings'])
 
-# np.save('temp/embeded_tweets.npy', embedded_tweets.to_numpy())
+# pca = PCA(n_components=embeddings_vector_length)
+# emb_table_reduced = pd.DataFrame(pca.fit_transform([*emb_table['embeddings']]))
 
-embedded_tweets = np.load('temp/embeded_tweets.npy')
+# #   ugly and not readable as fuck
+# agg_func = lambda tweets: [np.array([emb_table_reduced.loc[tkn][:].values for tkn in t ]) for t in tweets]
+# embedded_tweets = twt_data[['embeding_keys_array','tft.time_idx']].groupby('tft.time_idx').agg(agg_func) # group tweets by coin time index and embeed text
+# embedded_tweets = np.array([np.array(train[0]) for train in embedded_tweets.values ]) # simply convert nested lists to np.arrays,  
+
+# np.save('temp/embeded_tweets_length_%s.npy'%embeddings_vector_length, embedded_tweets)
+
+
+print('read embedded tweets')
+embedded_tweets = np.load('temp/embeded_tweets_length_%s.npy'%embeddings_vector_length)
 
 
 import keras
@@ -96,20 +113,13 @@ from keras.layers import ConvLSTM2D, Conv1D, AveragePooling1D, Dense
 from keras.models import Model
 from keras import backend
 
-conv_layer_1 = Conv1D(64, # filters
-                      4, # kernel_size,
-                      input_shape = (None,1),
-                      strides=1,
-                      padding='valid', #'causal',
-                      data_format='channels_last',
-                      dilation_rate=1,
-                      activation='relu', # maybe softplus,
-                      name = 'conv_layer_1'
-                     )
+input_tensor = tf.placeholder(name='input_tensor',
+                              dtype=tf.float32,
+                              shape=(batch_length, time_sequence_length, None, None, embeddings_vector_length))
 
-conv_lstm_layer = ConvLSTM2D(64, # filters
-                             4, # kernel_size,
-                             input_shape = (None,1),
+conv_lstm_layer = ConvLSTM2D(n_cnn_filters, # filters
+                             embedings_kernel_size, # kernel_size,
+#                             input_shape = (batch_length, time_sequence_length, None, None, embeddings_vector_length),
                              strides=1,
                              padding='valid', #'causal',
                              data_format='channels_last',
@@ -128,22 +138,18 @@ pool_layer_1 = AveragePooling1D(pool_size=4,
                                 data_format='channels_last',
                                 name = 'pool_layer_1'
                                 )
-                                
 
 
-# test model
-# test_input = keras.backend.expand_dims(np.array([np.array(x[:20], dtype=np.float32) for x in embedded_tweets[:3]]),-1)
-# conv_out = conv_layer_1(test_input)
-# pool_out = pool_layer_1(conv_out)
+num_batches = int(len(embedded_tweets) / batch_length)
+
+data_func = lambda batch, time: embedded_tweets[:batch*time].flatten().reshape(batch,time)
 
 
-test_input = backend.expand_dims(np.array([np.array(x[:20], dtype=np.float32) for x in embedded_tweets[:3]]),-1)
+tst_data = data_func(batch_length,time_sequence_length)
 
-clstm_out = conv_lstm_layer(test_input)
 
 
 # model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output, auxiliary_output])
-
 
 # trainable_count = int(
 #     np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
@@ -159,10 +165,12 @@ clstm_out = conv_lstm_layer(test_input)
 init = tf.global_variables_initializer()
 with tf.Session() as sn:
     init.run()
+    tst = sn.run(conv_lstm_layer, feed_dict={input_tensor:tst_data})
 
-    inp = test_input.eval() 
-    cnv = conv_out.eval()
-    pol = pool_out.eval()
+    
+    # inp = test_input.eval() 
+    # cnv = conv_out.eval()
+    # pol = pool_out.eval()
     
 
 # TODO: pen and paper exercise for one example (this is very important)
